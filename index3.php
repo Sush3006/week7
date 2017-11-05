8429d48ab64d7a608b05d6a8e38c40ad54f937eb
sushmitha sudarsan <ss3598@afsconnect1.njit.edu>
2017-11-05 10:58:29 -0500

    index3.php is added

---
+++ index3.php
@@ -0,0 +1,47 @@
+<?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+
+  class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
+
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+  } 
+  $servername = "sql1.njit.edu";
+  $username = "ss3598";
+  $password = "owlnerZ1g";
+  $dbname = "ss3598";
+
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    $stmt = $conn->prepare("SELECT * FROM accounts WHERE id < 6"); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+        echo $v;
+    }
+  }
+  catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+    echo "Connection failed: " . $e->getMessage();
+    
+  }
+  $conn = null;
+  echo "<br></table>";
+?>
